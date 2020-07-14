a9a59cdd33b99b4450d6ecda14ba6af92606e3f4
hawiwo <67214812+hawiwo@users.noreply.github.com>
2020-07-14 13:57:00 +0200

    Update aa_script.sh

--- aa_script.sh
+++ aa_script.sh
@@ -22,7 +22,7 @@ self_update() {
         # Now exit this old instance
         exit 1
     else
-        echo "Already the latest version...."
+        echo "Already the latest version......"
     fi
 }
 
