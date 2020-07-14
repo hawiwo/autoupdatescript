3c39529a037475812e5b6e992e0b988c48098155
hawiwo <67214812+hawiwo@users.noreply.github.com>
2020-07-14 13:55:20 +0200

    Update aa_script.sh

--- aa_script.sh
+++ aa_script.sh
@@ -13,7 +13,7 @@ self_update() {
 
     if [[ -n $(git diff --name-only origin/${BRANCH} | grep ${SCRIPTNAME}) ]] 
     then
-        echo "Found a new version of me, updating myself...."
+        echo "Found a new version of me, updating myself....."
         git pull --force
         git checkout ${BRANCH}
         git pull --force
