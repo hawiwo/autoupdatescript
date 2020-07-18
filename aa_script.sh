b6a41cef6834c3fb4c34a23078b2a5cda68f0fc0
hawiwo <67214812+hawiwo@users.noreply.github.com>
2020-07-18 19:08:41 +0200

    Update aa_script.sh

--- aa_script.sh
+++ aa_script.sh
@@ -28,7 +28,9 @@ self_update() {
 main() {
    echo "Running with ${@}"  
    if [ -z "$PS1" ]; then
-    echo "$(date +"%Y-%m-%d %H:%M") cron updated aa_script" >> cron.log
+    echo "$(date +"%Y-%m-%d %H:%M") cron updated aa_script" >> cron.log    
+    # crontab -l | sed -E '/#.*updatethingspeak\.sh.*/s/^#//' | crontab -
+    # crontab -l | sed -E '/.*updatethingspeak\.sh.*/s/^/#/' | crontab -
    fi   
 }
 
